586329341074dc9d140cdb1da9573d21051184fa
202030138SanghyunChoi <shshchoi@naver.com>
2024-09-25 12:47:31 +0900

    week 4

--- src/app/test-routing/[id]/page.jsx
+++ src/app/test-routing/[id]/page.jsx
@@ -1,7 +1,11 @@
-export default function Id() {
+export default function Id(props) {
+  console.log(props);
   return (
     <>
-      <h1>Hello, !!</h1>
+      {/* 동적 라우팅으로의 params 중 [id] 값 */}
+      <h1>Hello, {props.params.id}!!</h1>
+      {/* 직접 전달 / ?country=korea URL의 제일 뒤에만 가능 */}
+      <h1>Your Country: {props.searchParams.country}</h1>
     </>
   );
 }
